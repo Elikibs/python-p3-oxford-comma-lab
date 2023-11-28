2a011e5fc1b310dd0cd9e2d44358dd2241a2682b
Elisha <elishakibet67@gmail.com>
2023-11-28 13:22:15 +0300

    Update oxford_comma.py

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    else:
+        return ", ".join(items[0:-1]) + " and " + items[-1]
+
+print(oxford_comma(["fiddleheads", "okra", "kohlrabi", "hello", "fine"]))
